f2282a0720d6f91f058e6d36f66816ec43328d86
Maya Barnes <maya8617@gmail.com>
2025-04-25 12:16:32 -0700

    Try only chromium again

--- e2e_playwright/st_audio_test.py
+++ e2e_playwright/st_audio_test.py
@@ -144,7 +144,7 @@ def test_audio_uses_unified_height(
     assert_snapshot(audio_element, name="st_audio-unified_height")
 
 
-@pytest.mark.skip_browser("webkit")
+@pytest.mark.only_browser("chromium")
 def test_audio_source_error_with_url(app: Page, app_port: int):
     """Test `st.audio` source error when data is a url."""
     # Ensure audio source request return a 404 status
@@ -169,7 +169,7 @@ def test_audio_source_error_with_url(app: Page, app_port: int):
     )
 
 
-@pytest.mark.skip_browser("webkit")
+@pytest.mark.only_browser("chromium")
 def test_audio_source_error_with_path(app: Page, app_port: int):
     """Test `st.audio` source error when data is path (media endpoint)."""
     # Ensure audio source request return a 404 status
