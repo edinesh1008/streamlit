0858ff158c2af9d70863eea940efbfb9f64bb958
Bob Nisco <bob.nisco@snowflake.com>
2025-07-24 13:13:35 -0700

    A few fixes for python type checking

--- e2e_playwright/mega_tester_app.py
+++ e2e_playwright/mega_tester_app.py
@@ -295,7 +295,11 @@ if st.toggle("Show Bokeh chart (has some issues)", False):
 
     x = [1, 2, 3, 4, 5]
     y = [6, 7, 2, 4, 5]
-    p = figure(title="simple line example", x_axis_label="x", y_axis_label="y")
+    p = figure(
+        title="simple line example",
+        x_axis_label="x",  # type: ignore[invalid-argument-type]
+        y_axis_label="y",  # type: ignore[invalid-argument-type]
+    )
     p.line(x, y, legend_label="Trend", line_width=2)
     st.bokeh_chart(p, use_container_width=True)
 
