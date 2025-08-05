9b4bcc686bfc82cf7db0d8223265a16ec58f1f9c
lukasmasuch <lukas.masuch@gmail.com>
2025-08-05 23:20:49 +0200

    Add stale handling test

---
+++ e2e_playwright/stale_handling.py
@@ -0,0 +1,40 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+
+import streamlit as st
+
+if st.checkbox("Transient Spinner", value=False):
+    placeholder = st.empty()
+
+    # Related issues:
+    # https://github.com/streamlit/streamlit/issues/9239
+    # https://github.com/streamlit/streamlit/issues/9239
+
+    if "has_ran" not in st.session_state:
+        with st.spinner("Spinner"):
+            time.sleep(2)
+            st.write("Some text")
+
+        st.session_state.has_ran = True
+
+    else:
+        st.write("Some text")
+        placeholder.write("Has rerun")
+        time.sleep(5)
+
+        del st.session_state.has_ran
+
+    st.button("Rerun")
---
+++ e2e_playwright/stale_handling_test.py
@@ -0,0 +1,40 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from playwright.sync_api import Page, expect
+
+from e2e_playwright.shared.app_utils import (
+    click_checkbox,
+    get_button,
+)
+
+
+def test_stale_element_handling(app: Page):
+    """Test that elements don't incorrectly get marked as stale after reruns."""
+    # Check the checkbox to enable the transient spinner functionality
+    # This automatically waits for the app to run
+    click_checkbox(app, "Transient Spinner")
+
+    # Verify that there is a single "Some text" message
+    expect(app.get_by_text("Some text")).to_have_count(1)
+
+    # Click the rerun button but don't wait for rerun:
+    button_element = get_button(app, "Rerun")
+    button_element.click()
+
+    expect(app.get_by_text("Has rerun")).to_be_visible()
+
+    # The key test: verify that there is still a single "Some text" message
+    # In the previous issue, two messages would be shown with one of them being stale.
+    expect(app.get_by_text("Some text")).not_to_have_count(2)
