eeaa43b9ecc55839d30cc87e745180fae166a0ac
lukasmasuch <lukas.masuch@gmail.com>
2025-08-06 03:14:56 +0200

    Update

--- e2e_playwright/selectbox_playground.py
+++ e2e_playwright/selectbox_playground.py
@@ -1,3 +1,19 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022-2025)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
 import json
 
 import streamlit as st
@@ -14,7 +30,7 @@ with st.sidebar:
 
     placeholder = st.text_input("placeholder", "Choose an option")
     if not placeholder:
-        placeholder = None
+        placeholder = None  # type: ignore
 
     disabled = st.checkbox("disabled", False)
     label_visibility = st.radio(
@@ -33,6 +49,8 @@ with st.sidebar:
             "JSON",
         ],
     )
+
+    options: list[str | int | float | bool | None | tuple[str, int] | list[str]] = []
 
     if options_type == "List of strings":
         options = ["Apple", "Banana", "Orange", "Strawberry"]
@@ -56,14 +74,13 @@ with st.sidebar:
 
     st.subheader("Index & Default Value")
 
+    index: int | None = None
     use_index = st.radio("Use index?", ["Yes", "No (use placeholder)"], index=0)
     if use_index == "Yes":
         max_index = len(options) - 1 if options else 0
         index = st.number_input(
             "index", min_value=0, max_value=max_index, value=0, step=1
         )
-    else:
-        index = None
 
     st.subheader("Format Function")
     format_func_choice = st.radio(
