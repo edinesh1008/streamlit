f09557cac124e5c09765c94b70b2d02308acf741
lukasmasuch <lukas.masuch@gmail.com>
2025-09-03 18:58:00 +0200

    Add echo

--- e2e_playwright/st_markdown.py
+++ e2e_playwright/st_markdown.py
@@ -344,27 +344,28 @@ A[Start] --> B[Stop]
 ```
 """)
 
-st.markdown("""
-```mermaid
-mindmap
-  root((mindmap))
-    Origins
-      Long history
-      ::icon(fa fa-book)
-      Popularisation
-        British popular psychology author Tony Buzan
-    Research
-      On effectiveness<br/>and features
-      On Automatic creation
-        Uses
-            Creative techniques
-            Strategic planning
-            Argument mapping
-    Tools
-      Pen and paper
-      Mermaid
-```
-""")
+with st.echo():
+    st.markdown("""
+    ```mermaid
+    mindmap
+    root((mindmap))
+        Origins
+        Long history
+        ::icon(fa fa-book)
+        Popularisation
+            British popular psychology author Tony Buzan
+        Research
+        On effectiveness<br/>and features
+        On Automatic creation
+            Uses
+                Creative techniques
+                Strategic planning
+                Argument mapping
+        Tools
+        Pen and paper
+        Mermaid
+    ```
+    """)
 
 st.markdown("""
 ```mermaid
